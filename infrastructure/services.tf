cd38af4561256ae814205f1aa3210a151ca21bab
Vincent Mahnke <github@vincent.mahn.ke>
2025-10-18 07:50:57 +0200

    feat: Adds umap oauth client

--- infrastructure/services.tf
+++ infrastructure/services.tf
@@ -35,6 +35,9 @@ locals {
       "paperless" = {
         display_name = "Paperless"
       }
+      "umap" = {
+        display_name = "umap"
+      }
       "photos" = {
         display_name = "Immich"
         valid_redirect_urls = [
